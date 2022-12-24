67cb85c275701c9a74c6d0943dddb046f71fb10e
James Smith <james.smith@djalbat.com>
2022-12-24 12:15:41 +0000

    Exported the queue.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
 export { default as Task } from "./task";
+export { default as Queue } from "./queue";
 export { default as Scheduler } from "./scheduler";
 export { default as controller } from "./controller";
